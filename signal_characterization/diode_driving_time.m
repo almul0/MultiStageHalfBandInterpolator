6feb778c8dd3cc26b9cd6ebca02b569fafa68ded
Alberto Mur Lopez <565825@unizar.es>
2018-10-25 18:55:18 +0200

    Return diode driving time locs

--- signal_characterization/diode_driving_time.m
+++ signal_characterization/diode_driving_time.m
@@ -1,14 +1,14 @@
-function [tdhn, tdln] = diode_driving_time(zcl, tsnbhl, tsnbll)
-    tdhn = zeros(size(tsnbhl));
-    tdln = zeros(size(tsnbhl));
+function [tdh_locs, tdl_locs] = diode_driving_time(zcl, tsnbhl, tsnbll)
+    tdh_locs = zeros(size(tsnbhl));
+    tdl_locs = zeros(size(tsnbhl));
     for i=1:numel(tsnbhl)        
         if ( find(zcl>tsnbhl(i),1)) 
-            tdhn(i) = zcl(find(zcl>tsnbhl(i),1))-tsnbhl(i);
+            tdh_locs(i) = zcl(find(zcl>tsnbhl(i),1));
         end
     end
     for i=1:numel(tsnbll)
         if ( find(zcl>tsnbll(i),1)) 
-            tdln(i) = zcl(find(zcl>tsnbll(i),1))-tsnbll(i);
+            tdl_locs(i) = zcl(find(zcl>tsnbll(i),1));
         end
     end
 end
