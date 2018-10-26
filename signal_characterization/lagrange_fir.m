b243277fec207fc249d7ef826436d7dce118a7e3
Alberto Mur Lopez <565825@unizar.es>
2018-10-26 12:19:15 +0200

    Lagrange filter signal comparison

--- signal_characterization/lagrange_fir.m
+++ signal_characterization/lagrange_fir.m
@@ -24,19 +24,6 @@ for i=1:length(n)
 %    pause
 end
 
-%%
-fsig = figure;
-plot(data.osc.t, data.osc.il, 'k', 'DisplayName', sprintf('%.4fMHz', data.osc.fs/1e6));
-hold on
-plot(data.adc.t,data.adc.il, 'Marker', '.', 'LineStyle','none', 'DisplayName', 'Source')
-plot(data.dst.t, data.dst.il, ':b', 'LineWidth', 2, 'DisplayName', sprintf('%.4fMHz', data.dst.fs/1e6))
-
-figure(fsig)
-plot(data.int.t,data.int.il,'r','DisplayName',sprintf('Q = %d ',n(i)+1))
-figure(fsig)
-xlabel(data.t_str)
-legend
-figure(fig_freqz)
-legend('Ideal','firpm Design')
-xlabel 'Radian Frequency (\omega/\pi)', ylabel 'Magnitude'
-ylabel('Amplitude')
+data.int.label = sprintf('Lagrange M = %d ', n(i)+1);
+interpolation_freq_spectra(data);
+interpolation_signal_comparison(data);
