115d9e52232fd7cfafb8e936b061afca24668db8
Alberto Mur Lopez <565825@unizar.es>
2019-01-10 19:22:18 +0100

    Reindent and changed to axi4-stream

--- VHDL/MultiStageHalfBandInterpolator/MultiStageHalfBandInterpolator.srcs/sources_1/new/MultiStageHalfBandInterpolator.vhd
+++ VHDL/MultiStageHalfBandInterpolator/MultiStageHalfBandInterpolator.srcs/sources_1/new/MultiStageHalfBandInterpolator.vhd
@@ -37,11 +37,11 @@ use work.MultiStageHalfBandInterpolator_param.all;
 entity MultiStageHalfBandInterpolator is
 --  Port ( );
 	Port(
-		SlaveAxi_RI : in GLOBAL2SAXILITE;
-		SlaveAxi_RO : out SAXILITE2GLOBAL;
+		SlaveAxi_RI : in GLOBAL2SAXIS;
+		SlaveAxi_RO : out SAXIS2GLOBAL;
 		
-		MasterAxi_RI : in GLOBAL2MAXILITE;
-		MasterAxi_RO : out MAXILITE2GLOBAL
+		MasterAxi_RI : in GLOBAL2MAXIS;
+		MasterAxi_RO : out MAXIS2GLOBAL
 	);
 end MultiStageHalfBandInterpolator;
 
@@ -54,11 +54,11 @@ architecture Behavioral of MultiStageHalfBandInterpolator is
 			 COEFF2: Coeff
 	     );
 	PORT(
-			SlaveAxi_RI : in GLOBAL2SAXILITE;
-			SlaveAxi_RO : out SAXILITE2GLOBAL;
+			SlaveAxi_RI : in GLOBAL2SAXIS;
+			SlaveAxi_RO : out SAXIS2GLOBAL;
 			
-			MasterAxi_RI : in GLOBAL2MAXILITE;
-			MasterAxi_RO : out MAXILITE2GLOBAL
+			MasterAxi_RI : in GLOBAL2MAXIS;
+			MasterAxi_RO : out MAXIS2GLOBAL
 			);
 	END COMPONENT;
 	COMPONENT HalfBandFilterTwoTaps
@@ -68,19 +68,19 @@ architecture Behavioral of MultiStageHalfBandInterpolator is
 				 COEFF1: Coeff
 		     );
 		PORT(
-				SlaveAxi_RI : in GLOBAL2SAXILITE;
-				SlaveAxi_RO : out SAXILITE2GLOBAL;
+				SlaveAxi_RI : in GLOBAL2SAXIS;
+				SlaveAxi_RO : out SAXIS2GLOBAL;
 				
-				MasterAxi_RI : in GLOBAL2MAXILITE;
-				MasterAxi_RO : out MAXILITE2GLOBAL
+				MasterAxi_RI : in GLOBAL2MAXIS;
+				MasterAxi_RO : out MAXIS2GLOBAL
 				);
 		END COMPONENT;
 	
-	signal Master03Axi_RI : GLOBAL2MAXILITE;
-	signal Master03Axi_RO : MAXILITE2GLOBAL;
+	signal Master03Axi_RI : GLOBAL2MAXIS;
+	signal Master03Axi_RO : MAXIS2GLOBAL;
 	
-	signal Master02Axi_RI : GLOBAL2MAXILITE;
-	signal Master02Axi_RO : MAXILITE2GLOBAL;
+	signal Master02Axi_RI : GLOBAL2MAXIS;
+	signal Master02Axi_RO : MAXIS2GLOBAL;
 	
 begin
 	HB3: HalfBandFilterThreeTaps
@@ -93,25 +93,17 @@ begin
 	PORT MAP(
 		SlaveAxi_RI.s_axi_aclk     => SlaveAxi_RI.s_axi_aclk,
 		SlaveAxi_RI.s_axi_aresetn  => SlaveAxi_RI.s_axi_aresetn,
-		SlaveAxi_RI.s_axi_wdata    => SlaveAxi_RI.s_axi_wdata,
-		SlaveAxi_RI.s_axi_wstrb    => SlaveAxi_RI.s_axi_wstrb,
-		SlaveAxi_RI.s_axi_wvalid   => SlaveAxi_RI.s_axi_wvalid,
-		SlaveAxi_RI.s_axi_bready   => SlaveAxi_RI.s_axi_bready,
+		SlaveAxi_RI.s_axi_tdata    => SlaveAxi_RI.s_axi_tdata,
+		SlaveAxi_RI.s_axi_tvalid   => SlaveAxi_RI.s_axi_tvalid,
 
-		SlaveAxi_RO.s_axi_wready   => SlaveAxi_RO.s_axi_wready,
-		SlaveAxi_RO.s_axi_bresp    => SlaveAxi_RO.s_axi_bresp,
-		SlaveAxi_RO.s_axi_bvalid   => SlaveAxi_RO.s_axi_bvalid,	
+		SlaveAxi_RO.s_axi_tready   => SlaveAxi_RO.s_axi_tready,
 		
 		MasterAxi_RO.m_axi_aclk    => Master03Axi_RO.m_axi_aclk,
 		MasterAxi_RO.m_axi_aresetn => Master03Axi_RO.m_axi_aresetn,
-		MasterAxi_RO.m_axi_wdata   => Master03Axi_RO.m_axi_wdata,
-		MasterAxi_RO.m_axi_wstrb   => Master03Axi_RO.m_axi_wstrb,
-		MasterAxi_RO.m_axi_wvalid  => Master03Axi_RO.m_axi_wvalid,
-		MasterAxi_RO.m_axi_bready  => Master03Axi_RO.m_axi_bready,
+		MasterAxi_RO.m_axi_tdata   => Master03Axi_RO.m_axi_tdata,
+		MasterAxi_RO.m_axi_tvalid  => Master03Axi_RO.m_axi_tvalid,
 
-		MasterAxi_RI.m_axi_wready  => Master03Axi_RI.m_axi_wready,
-		MasterAxi_RI.m_axi_bresp   => Master03Axi_RI.m_axi_bresp,
-		MasterAxi_RI.m_axi_bvalid  => Master03Axi_RI.m_axi_bvalid	
+		MasterAxi_RI.m_axi_tready  => Master03Axi_RI.m_axi_tready
 	);
 	
 	HB2: HalfBandFilterThreeTaps
@@ -124,25 +116,17 @@ begin
 		PORT MAP(
 			SlaveAxi_RI.s_axi_aclk     => Master03Axi_RO.m_axi_aclk,
 			SlaveAxi_RI.s_axi_aresetn  => Master03Axi_RO.m_axi_aresetn,
-			SlaveAxi_RI.s_axi_wdata    => Master03Axi_RO.m_axi_wdata,
-			SlaveAxi_RI.s_axi_wstrb    => Master03Axi_RO.m_axi_wstrb,
-			SlaveAxi_RI.s_axi_wvalid   => Master03Axi_RO.m_axi_wvalid,
-			SlaveAxi_RI.s_axi_bready   => Master03Axi_RO.m_axi_bready,
+			SlaveAxi_RI.s_axi_tdata    => Master03Axi_RO.m_axi_tdata,
+			SlaveAxi_RI.s_axi_tvalid   => Master03Axi_RO.m_axi_tvalid,
 	
-			SlaveAxi_RO.s_axi_wready   => Master03Axi_RI.m_axi_wready,
-			SlaveAxi_RO.s_axi_bresp    => Master03Axi_RI.m_axi_bresp,
-			SlaveAxi_RO.s_axi_bvalid   => Master03Axi_RI.m_axi_bvalid,	
+			SlaveAxi_RO.s_axi_tready   => Master03Axi_RI.m_axi_tready,
 			
 			MasterAxi_RO.m_axi_aclk    => Master02Axi_RO.m_axi_aclk,
 			MasterAxi_RO.m_axi_aresetn => Master02Axi_RO.m_axi_aresetn,
-			MasterAxi_RO.m_axi_wdata   => Master02Axi_RO.m_axi_wdata,
-			MasterAxi_RO.m_axi_wstrb   => Master02Axi_RO.m_axi_wstrb,
-			MasterAxi_RO.m_axi_wvalid  => Master02Axi_RO.m_axi_wvalid,
-			MasterAxi_RO.m_axi_bready  => Master02Axi_RO.m_axi_bready,
+			MasterAxi_RO.m_axi_tdata   => Master02Axi_RO.m_axi_tdata,
+			MasterAxi_RO.m_axi_tvalid  => Master02Axi_RO.m_axi_tvalid,
 
-			MasterAxi_RI.m_axi_wready  => Master02Axi_RI.m_axi_wready,
-			MasterAxi_RI.m_axi_bresp   => Master02Axi_RI.m_axi_bresp,
-			MasterAxi_RI.m_axi_bvalid  => Master02Axi_RI.m_axi_bvalid	
+			MasterAxi_RI.m_axi_tready  => Master02Axi_RI.m_axi_tready
 			
 		);
 		
@@ -155,25 +139,17 @@ begin
 			PORT MAP(
 				SlaveAxi_RI.s_axi_aclk     => Master02Axi_RO.m_axi_aclk,
 				SlaveAxi_RI.s_axi_aresetn  => Master02Axi_RO.m_axi_aresetn,
-				SlaveAxi_RI.s_axi_wdata    => Master02Axi_RO.m_axi_wdata,
-				SlaveAxi_RI.s_axi_wstrb    => Master02Axi_RO.m_axi_wstrb,
-				SlaveAxi_RI.s_axi_wvalid   => Master02Axi_RO.m_axi_wvalid,
-				SlaveAxi_RI.s_axi_bready   => Master02Axi_RO.m_axi_bready,
+				SlaveAxi_RI.s_axi_tdata    => Master02Axi_RO.m_axi_tdata,
+				SlaveAxi_RI.s_axi_tvalid   => Master02Axi_RO.m_axi_tvalid,
 		
-				SlaveAxi_RO.s_axi_wready   => Master02Axi_RI.m_axi_wready,
-				SlaveAxi_RO.s_axi_bresp    => Master02Axi_RI.m_axi_bresp,
-				SlaveAxi_RO.s_axi_bvalid   => Master02Axi_RI.m_axi_bvalid,	
+				SlaveAxi_RO.s_axi_tready   => Master02Axi_RI.m_axi_tready,
 				
 				MasterAxi_RO.m_axi_aclk    => MasterAxi_RO.m_axi_aclk,
 				MasterAxi_RO.m_axi_aresetn => MasterAxi_RO.m_axi_aresetn,
-				MasterAxi_RO.m_axi_wdata   => MasterAxi_RO.m_axi_wdata,
-				MasterAxi_RO.m_axi_wstrb   => MasterAxi_RO.m_axi_wstrb,
-				MasterAxi_RO.m_axi_wvalid  => MasterAxi_RO.m_axi_wvalid,
-				MasterAxi_RO.m_axi_bready  => MasterAxi_RO.m_axi_bready,
+				MasterAxi_RO.m_axi_tdata   => MasterAxi_RO.m_axi_tdata,
+				MasterAxi_RO.m_axi_tvalid  => MasterAxi_RO.m_axi_tvalid,
 	
-				MasterAxi_RI.m_axi_wready  => MasterAxi_RI.m_axi_wready,
-				MasterAxi_RI.m_axi_bresp   => MasterAxi_RI.m_axi_bresp,
-				MasterAxi_RI.m_axi_bvalid  => MasterAxi_RI.m_axi_bvalid	
+				MasterAxi_RI.m_axi_tready  => MasterAxi_RI.m_axi_tready
 			);
 	
 end Behavioral;
